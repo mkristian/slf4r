d4f372294579ce8fd95a7fb4ece16f41d9df90cd
mkristian <m.kristian@web.de>
2010-06-28 18:30:25 +0530

    fixed non java case to fall back to rubylogger

--- lib/slf4r.rb
+++ lib/slf4r.rb
@@ -1,6 +1,6 @@
 require 'slf4r/logger'
 begin
   require 'slf4r/java_logger'
-rescue
+rescue LoadError
   require 'slf4r/ruby_logger'
 end
--- lib/slf4r/version.rb
+++ lib/slf4r/version.rb
@@ -1,3 +1,3 @@
 module Slf4r
-  VERSION = '0.3.1'
+  VERSION = '0.3.2'
 end
