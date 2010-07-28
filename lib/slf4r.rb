3a9343289a8edfe4d5ded0130e0200c9bdac4c3c
mkristian <m.kristian@web.de>
2010-07-28 13:57:09 +0530

    better decision on which logger to use as default: ruby, slf4j, etc

--- lib/slf4r.rb
+++ lib/slf4r.rb
@@ -1,6 +1,17 @@
 require 'slf4r/logger'
 begin
+  # make sure we have java
+  require 'java'
+  begin
+    # make sure we have SLF4J in the classloader
+    import 'org.slf4j.LoggerFactory'
+  rescue NameError
+    puts "no SLF4J found in classloader - using ruby logger"
+    require 'slf4r/ruby_logger'
+  end
   require 'slf4r/java_logger'
+  puts "using slf4j logger"
 rescue LoadError
+  puts "using ruby logger"
   require 'slf4r/ruby_logger'
 end
--- lib/slf4r/version.rb
+++ lib/slf4r/version.rb
@@ -1,3 +1,3 @@
 module Slf4r
-  VERSION = '0.3.2'
+  VERSION = '0.3.2'.freeze
 end
